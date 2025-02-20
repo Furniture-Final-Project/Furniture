0fa6e9dc99be5723c2a249f6dd173f2bddb6f821
Rotem-55 <rotem880@gmail.com>
2025-02-20 19:33:13 +0200

    nodyfied __init__.py

--- source/models/__init__.py
+++ source/models/__init__.py
@@ -1,3 +1,8 @@
 from .Furniture import Furniture, Chair, Table, Sofa, BookShelf, Bed
 
+
 __all__ = ["Furniture", "Chair", "Table", "Sofa", "BookShelf", "Bed"]
+
+
+def inventory():
+    return None
