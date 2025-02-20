9df9651bdc51c5c06dd329532f4e914cf82f4f64
Rotem-55 <rotem880@gmail.com>
2025-02-20 19:32:25 +0200

    added app.py file

---
+++ app.py
@@ -0,0 +1,19 @@
+from source.models.inventory import Inventory
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+# Initialize the Inventory instance at startup
+data_folder = os.path.join("source", "database")
+inventory_instance = Inventory(data_folder)
+
+
+@app.route('/available-items', methods=['GET'])
+def get_available_items():
+    items = inventory_instance.get_all_available_items()
+    return jsonify(items)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
