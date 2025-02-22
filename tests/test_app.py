9df3fd20c39ac6cdd4f0cfd373dc0bbeaf5c3edd
Rotem-55 <rotem880@gmail.com>
2025-02-22 18:44:17 +0200

    'renaming'

--- tests/test_app.py
+++ tests/test_app.py
@@ -160,7 +160,7 @@ def test_user_get_all_items(client):
                     }
                 }
 
-def test_filtered_items(client):
+def test_single_filter(client):
     response = client.get('/items', query_string={"category": "Bed"})
     assert response.status_code == http.HTTPStatus.OK
     data = response.get_json()
