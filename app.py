71182c6b57491c7546f85c45229264820d1881ee
Rotem-55 <rotem880@gmail.com>
2025-02-22 19:31:13 +0200

    filter by price

--- app.py
+++ app.py
@@ -13,12 +13,18 @@ def create_app(config: dict):
         s = schema.session()
         query = s.query(schema.Furniture)
         category = flask.request.args.get('category')
+        price = flask.request.args.get('max_price')
         if category is not None:
             query = query.filter_by(category=category)
+        if price is not None:
+            price = int(price)
+            query = query.filter(schema.Furniture.price<price)
             
         results = query.all()
         items = { result.model_num: result.to_dict() for result in results}
         return flask.jsonify({'items': items})
 
-    
     return app
+    
+    
+
